49cedba70343d72dbd28db92be4c98a74c0f5f25
Donato Cardoso <donatocardoso11@gmail.com>
2021-05-04 19:35:58 -0300

    feature: adicionando monitoramento e identificacao da ferramenta

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
+require('./src/utils/monitory');
 
+const chalk = require('chalk');
 const programa = require('./src/pt-br/principal');
 
+const colunas = process.stdout.columns;
+
+console.log(chalk.bold(''.padStart(colunas, '*')));
+console.log(chalk.bold('BUSCA  CLI'.padStart(colunas / 2).padEnd(colunas)));
+console.log(chalk.bold(''.padStart(colunas, '*')));
+console.log();
+
 programa.parse(process.argv, programa.opts());
+
+process.on('exit', () => console.log());
---
+++ src/utils/monitory.js
@@ -0,0 +1,42 @@
+const Sentry = require('@sentry/node');
+const { mostraMensagem } = require('./message');
+
+process.on('uncaughtException', (err) => {
+  mostraMensagem(
+    'Ops!! Ocorreu uma falha não esperada!',
+    '',
+    'Um aviso foi enviado para nossos desenvolvedores, fique tranquilo(a) eles vão corrigir o mais rápido possível.',
+    '',
+    `O que aconteceu foi: ${err.message}`
+  );
+
+  Sentry.init({
+    dsn: 'https://8e3b9869e8c74b93bddaf17a5b378592@o613678.ingest.sentry.io/5749414',
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
+  const transaction = Sentry.startTransaction({
+    op: 'monitory',
+    name: 'uncaughtException',
+  });
+
+  Sentry.configureScope((scope) => {
+    scope.setLevel('error');
+    scope.setTag('mechanism', 'generic');
+
+    scope.setContext('parametros', {
+      date: new Date(),
+      argv: process.argv,
+    });
+  });
+
+  Sentry.captureException(err, {});
+
+  transaction.finish();
+
+  Sentry.close().then(() => process.exit(1));
+});
